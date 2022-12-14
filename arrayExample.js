838485661ef55a4c7bece11145a05a59d6b7818c
Connor Ferris <ferr0373@gmail.com>
2022-12-14 16:13:35 -0600

    Array Example commands added

--- arrayExample.js
+++ arrayExample.js
@@ -1,3 +1,14 @@
 console.log ( 'Arrays Example');
 
-let bagel = [];
+let bagel = [ 'bacon', 'lettuce', 'tomato'];
+
+bagel.indexOf ( 'mustard');
+bagel.indexOf ( 'tomato');
+
+bagel.push ( 'mustard' );
+bagel.pop();
+bagel.push('mayonaise')
+bagel.push ('asiago')
+bagel.unshift('asiago');
+bagel.shift();  
+bagel.unshift('sesame');
